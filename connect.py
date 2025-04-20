2fa67bf28729a29fb06a2e2d643ffe68edfd20a2
Илья Соловьев <ilsolovev@MacBook-Air-Ila.local>
2025-04-20 15:53:23 +0300

    add Reddis connection file

--- connect.py
+++ connect.py
@@ -1,6 +1,7 @@
 import psycopg2
 import os
 from dotenv import load_dotenv
+import redis
 
 load_dotenv()
 
@@ -12,3 +13,16 @@ def get_connection():
         host=os.getenv("DB_HOST"),
         port=os.getenv("DB_PORT")
     )
+
+
+def get_redis_connection() -> redis.Redis:
+    return redis.Redis(
+        host=os.getenv("REDIS_HOST"),
+        port=int(os.getenv("REDIS_PORT")),
+        password=os.getenv("REDIS_PASSWORD")
+    )
+
+if __name__ == "__main__":
+    r = get_redis_connection()
+    r.set("foo", "bar")
+    print(r.get("foo").decode())
